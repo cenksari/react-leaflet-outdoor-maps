30d4c38f489acdaccca0e04c77d02c963dfe787b
Cenk SARI <cenk@cenksari.com>
2024-06-15 11:23:34 +0100

    responsive fix

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -54,60 +54,8 @@ const Legend = ({ map, logo, data, locations }: IProps): React.JSX.Element => {
 
   return (
     <div className='legend'>
-      <div className='autocomplete-container'>
-        <div className='flex flex-gap-small flex-v-center search-area'>
-          <button type='button' onClick={() => inputRef.current?.focus()}>
-            <span className='material-symbols-outlined input-icon'>search</span>
-          </button>
-          <input
-            type='text'
-            id='search'
-            name='search'
-            maxLength={32}
-            ref={inputRef}
-            value={keyword}
-            onChange={handleChange}
-            placeholder='Search places...'
-          />
-          {keyword && (
-            <button
-              type='button'
-              className='pointer active-opacity'
-              onClick={() => handleAutocompleterClose()}
-            >
-              <span className='material-symbols-outlined input-icon'>close</span>
-            </button>
-          )}
-        </div>
-
-        {filteredResults && (
-          <div className='autocomplete scroller-vertical'>
-            {filteredResults.length > 0 ? (
-              <ul>
-                {filteredResults.map((item) => (
-                  <li key={item.id}>
-                    <button
-                      type='button'
-                      onClick={() => handleClick(item.shapeCoords[0])}
-                      className='flex-space-between flex-gap flex-h-center pointer active-opacity'
-                    >
-                      <span className='material-symbols-outlined'>{item.category.icon}</span>
-                      <span className='flex flex-grow flex-self'>{item.title}</span>
-                      <span className='material-symbols-outlined'>center_focus_weak</span>
-                      <em>Show</em>
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>No results found!</p>
-            )}
-          </div>
-        )}
-      </div>
-
-      <div className='flex flex-space-between legend-header'>
-        <h5>Legend</h5>
+      <div className='flex flex-space-between flex-v-center legend-header'>
+        <h5>Legend & Search</h5>
         <button type='button' onClick={() => setClose(!close)}>
           <span className='material-symbols-outlined down-icon pointer active-opacity'>
             {close ? 'expand_less' : 'expand_more'}
@@ -116,22 +64,76 @@ const Legend = ({ map, logo, data, locations }: IProps): React.JSX.Element => {
       </div>
 
       {!close && (
-        <div className='flex flex-space-between legend-content'>
-          <div className='grid flex-gap'>
-            {data?.map((item) => (
-              <div key={item.id} className='flex flex-v-center flex-gap'>
-                <span className='material-symbols-outlined'>{item.icon}</span>
-                <em>{item.name}</em>
+        <>
+          <div className='autocomplete-container'>
+            <div className='flex flex-gap-small flex-v-center search-area'>
+              <button type='button' onClick={() => inputRef.current?.focus()}>
+                <span className='material-symbols-outlined input-icon'>search</span>
+              </button>
+              <input
+                type='text'
+                id='search'
+                name='search'
+                maxLength={32}
+                ref={inputRef}
+                value={keyword}
+                onChange={handleChange}
+                placeholder='Search places...'
+              />
+              {keyword && (
+                <button
+                  type='button'
+                  className='pointer active-opacity'
+                  onClick={() => handleAutocompleterClose()}
+                >
+                  <span className='material-symbols-outlined input-icon'>close</span>
+                </button>
+              )}
+            </div>
+
+            {filteredResults && (
+              <div className='autocomplete scroller-vertical'>
+                {filteredResults.length > 0 ? (
+                  <ul>
+                    {filteredResults.map((item) => (
+                      <li key={item.id}>
+                        <button
+                          type='button'
+                          onClick={() => handleClick(item.shapeCoords[0])}
+                          className='flex-space-between flex-gap flex-h-center pointer active-opacity'
+                        >
+                          <span className='material-symbols-outlined'>{item.category.icon}</span>
+                          <span className='flex flex-grow flex-self'>{item.title}</span>
+                          <span className='material-symbols-outlined'>center_focus_weak</span>
+                          <em>Show</em>
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No results found!</p>
+                )}
               </div>
-            ))}
+            )}
           </div>
 
-          {logo && (
-            <div className='flex flex-v-end'>
-              <img src={logo} width='150' alt='FIA logo' draggable='false' />
+          <div className='flex flex-space-between legend-content'>
+            <div className='grid flex-gap'>
+              {data?.map((item) => (
+                <div key={item.id} className='flex flex-v-center flex-gap'>
+                  <span className='material-symbols-outlined'>{item.icon}</span>
+                  <em>{item.name}</em>
+                </div>
+              ))}
             </div>
-          )}
-        </div>
+
+            {logo && (
+              <div className='flex flex-v-end'>
+                <img src={logo} width='150' alt='FIA logo' draggable='false' />
+              </div>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
