4574eb03700ec78139ca33d5c3cbbd3530935fbe
Cenk SARI <cenk@cenksari.com>
2024-06-24 17:58:06 +0100

    eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,16 +7,13 @@ module.exports = {
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
-    'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:prettier/recommended',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'import', 'react', 'jsx-a11y', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'react/require-default-props': 'off',
